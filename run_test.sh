9c2ed01c0b090773cf5d164f957b95bc271e7e04
bruno <bruno.fernandos@yahoo.com>
2015-08-23 17:46:06 +0200

    run_test.sh : HTTP options

--- run_test.sh
+++ run_test.sh
@@ -15,11 +15,14 @@ NO_BROWSER_CHECK="--no-default-browser-check"
 DISABLE_CACHE="--disable-cache --disable-gpu-program-cache --disable-gpu-shader-disk-cache --disable-offline-load-stale-cache --disk-cache-size=0 --gpu-program-cache-size-kb=0 --media-cache-size=0 --disable-application-cache"
 
 OPTIONS="$INCOGNITO $IGNORE_CERTIFICATE $NO_BROWSER_CHECK" #$DISABLE_CACHE"
+PROTO_OPTIONS=
 
 BROWSER="chromium-browser"
 
 IP_ADDR=${1}
 URL=
+
+ERROR="${0} <IP_ADDR>"
 
 # Functions
 
@@ -30,8 +33,14 @@ function clear_cache {
 
 # main
 
-pkill chromium-browser
-pkill chromium
+if [ $# != 1 ]
+then
+    echo $ERROR
+    exit 1
+fi
+
+#pkill chromium-browser
+#pkill chromium
 
 for i in $(seq 0 $ARRAY_LENGTH)
 do
@@ -39,20 +48,23 @@ do
     if [ ${PROTOCOL[$i]} = "http" ]
     then
 	URL="http://${IP_ADDR}:${PORT[$i]}/player.html"
+	PROTO_OPTIONS="--use-spdy=off"
 	echo $URL
     elif [ ${PROTOCOL[$i]} = "https" ]
     then
 	URL="https://${IP_ADDR}:${PORT[$i]}/player.html"
+	PROTO_OPTIONS="--use-spdy=off"
 	echo $URL
     elif [ ${PROTOCOL[$i]} = "h2" ]
     then
 	URL="https://${IP_ADDR}:${PORT[$i]}/player.html"
+	PROTO_OPTIONS="--enable-spdy4"
 	echo $URL
     else
 	echo "Invalid protocol"
     fi
 
-    echo "chromium-browser $OPTIONS $URL"
+    echo "chromium-browser $OPTIONS $PROTO_OPTIONS $URL"
     #chromium-browser $OPTIONS $URL 
 done
 
